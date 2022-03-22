feaa5d9a9a1676958394129bd55fa7a302423bbd
itslitulinchpin <dlsdk0930@gmail.com>
2022-03-22 09:13:24 +0900

    Tag 화면에 띄우기_DB연동확인은 못한상태

--- hello-react/src/components/minicomponents/Righttop.js
+++ hello-react/src/components/minicomponents/Righttop.js
@@ -1,9 +1,29 @@
 import React, {Component} from'react';
-
+import axios from 'axios';
 
 class Righttop extends Component{
+
+    choose_tag_data(){ 
+        var searchingFile = document.getElementById("choose_tag_data").value; 
+        axios(
+            {
+              headers: {"Content-Type": "application/json"},
+              url: '/choose_tag',
+              method: 'post',
+              data: {
+                name: searchingFile
+              }, 
+              baseURL: 'http://localhost:8080'
+              //withCredentials: true
+            }
+          ).then(function (response) {
+            console.log(response.data)
+          });
+    }
+
+
     render(){
-        const st = {
+        {/*const st = {
             width:'525px',
             height:'35px',
             border:'0.01px',
@@ -12,15 +32,23 @@ class Righttop extends Component{
             textAlign:'center',
             backgroundColor:'white'
             
-        }
+        }*/}
         
         
         return(
-           <div style={st}>
-               
-               <h3> {this.props.tag}</h3>
+           <form onSubmit={ function(e){
+            e.preventDefault();
+                
+                this.props.onSubmit(e.target.id.value); 
+                this.choose_tag_data(e.target.id.value);
+                
+                
+                }.bind(this)}>
 
-           </div>
+                <div> <input  id = "choose_tag_data" name = "id" type = "submit"  value={this.props.id} ></input> </div>
+           </form>
+               
+        
 
         );
     }
